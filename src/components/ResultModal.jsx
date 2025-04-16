949bc9116e798902e03b8f7c009011fe48ab4dd4
Vince Chimento <vchimento@gmail.com>
2025-04-16 14:27:44 -0400

    display score, reset timer with esc key

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -6,10 +6,11 @@ export default function ResultModal({
   remainingTime,
   onReset,
 }) {
+  const dialog = useRef();
+
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
-
-  const dialog = useRef();
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -20,8 +21,9 @@ export default function ResultModal({
   });
 
   return (
-    <dialog ref={dialog} className='result-modal'>
+    <dialog ref={dialog} className='result-modal' onClose={onReset}>
       {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
