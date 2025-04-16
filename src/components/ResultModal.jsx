15e4a739cb433683038875300cc2c37c3121bffe
Vince Chimento <vchimento@gmail.com>
2025-04-16 13:40:49 -0400

    implement timeRemaining state, manage results and game reset

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,6 +1,14 @@
 import { useRef, useImperativeHandle } from 'react';
 
-export default function ResultModal({ ref, result, targetTime }) {
+export default function ResultModal({
+  ref,
+  targetTime,
+  remainingTime,
+  onReset,
+}) {
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   const dialog = useRef();
 
   useImperativeHandle(ref, () => {
@@ -13,14 +21,15 @@ export default function ResultModal({ ref, result, targetTime }) {
 
   return (
     <dialog ref={dialog} className='result-modal'>
-      <h2>You {result}</h2>
+      {userLost && <h2>You lost</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{' '}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method='dialog'>
+      <form method='dialog' onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
